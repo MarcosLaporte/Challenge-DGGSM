6c8f78c90e86f322fa1d399051314e79e4d87d36
LapMarcos <78131374+MarcosLaporte@users.noreply.github.com>
2025-02-20 20:15:41 -0300

    feat: execute app.listen only if file is run directly

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,11 @@ app.get("/", (req, res) => {
   res.send("Bienvenido a la API de empleados");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server ejecutando en http://localhost:${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server ejecutando en http://localhost:${PORT}`);
+  });
+}
 
 const cors = require("cors");
 app.use(cors({ origin: "http://localhost:4200" }));
